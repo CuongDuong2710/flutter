e5bbb451a9bcb290044bc2bbdad75deb7669b18b
cuongdnq2710 <cuong.duong@titancorpvn.com>
2018-11-18 11:34:10 +0700

    create stateful widget and state class

--- firtapp/lib/main.dart
+++ firtapp/lib/main.dart
@@ -3,10 +3,27 @@ import 'package:english_words/english_words.dart';
 
 void main() => runApp(new MyApp());
 
-class MyApp extends StatelessWidget {
+class RandomEnglishWords extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new RandomEnglishWordsState();
+  }
+}
+
+class RandomEnglishWordsState extends State<RandomEnglishWords> {
   @override
   Widget build(BuildContext context) {
     final wordPair = new WordPair.random();
+    return new Text(
+      wordPair.asUpperCase,
+      style: new TextStyle(fontSize: 22.0)
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return new MaterialApp(
       title: "This is my first Flutter App",
       home: new Scaffold(
@@ -14,12 +31,9 @@ class MyApp extends StatelessWidget {
           title: new Text("This is Header's title"),
         ),
         body: new Center(
-          child: new Text(
-            wordPair.asUpperCase,
-            style: new TextStyle(fontSize: 22.0),
+          child: new RandomEnglishWords()
           ),
         ),
-      ),
     );
   }
 }
