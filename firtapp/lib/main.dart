a42b4edd9bcbe9bd1a8b9598efb035778f2abccb
cuongdnq2710 <cuong.duong@titancorpvn.com>
2018-11-18 11:18:46 +0700

    install & import library to use wordpair

--- firtapp/lib/main.dart
+++ firtapp/lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
-void main() {
-  runApp(new MyApp());
-}
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
     return new MaterialApp(
       title: "This is my first Flutter App",
       home: new Scaffold(
@@ -14,7 +14,10 @@ class MyApp extends StatelessWidget {
           title: new Text("This is Header's title"),
         ),
         body: new Center(
-          child: new Text("This is at the body's center"),
+          child: new Text(
+            wordPair.asUpperCase,
+            style: new TextStyle(fontSize: 22.0),
+          ),
         ),
       ),
     );
