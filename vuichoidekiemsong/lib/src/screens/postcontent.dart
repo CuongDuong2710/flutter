1a3d93cd48a0aa56d779e759586e879974ad81b7
cuongdnq2710 <cuong.duong@titancorpvn.com>
2019-02-19 21:36:20 +0700

    get value of text/ set value of dropdownmenu

--- vuichoidekiemsong/lib/src/screens/postcontent.dart
+++ vuichoidekiemsong/lib/src/screens/postcontent.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+import '../entity/category.dart';
 import '../entity/post.dart';
 
 class PostContent extends StatefulWidget {
@@ -12,47 +13,52 @@ class PostContent extends StatefulWidget {
 }
 
 class PostContentState extends State<PostContent> {
+  final titleTextController = TextEditingController();
+  final contentTextController = TextEditingController();
+
   StreamSubscription<DocumentSnapshot> subscription;
   final CollectionReference collectionReference =
       Firestore.instance.collection("post");
 
   List _categories = [
-    "Vui chơi kiếm sống",
-    "Xây dựng thương hiệu cá nhân",
-    "Lò Copywriter"
+    Category("1", "Vui chơi kiếm sống", ""),
+    Category("2", "Xây dựng thương hiệu cá nhân", ""),
+    Category("3", "Lò Copywriter", "")
   ];
+
   List<DropdownMenuItem<String>> _dropDownMenuItems;
-  String _seletectedFruit;
+  String _seletectedCategory;
 
   @override
   void initState() {
     _dropDownMenuItems = _buildAndGetDropDownMenuItems(_categories);
-    _seletectedFruit = _dropDownMenuItems[0].value;
+    _seletectedCategory = _dropDownMenuItems[0].value;
     super.initState();
   }
 
   List<DropdownMenuItem<String>> _buildAndGetDropDownMenuItems(
       List categories) {
     List<DropdownMenuItem<String>> items = new List();
-    for (String category in categories) {
-      items.add(
-          new DropdownMenuItem(value: category, child: new Text(category)));
+    for (Category category in categories) {
+      items.add(new DropdownMenuItem(
+          value: category.id, child: new Text(category.name)));
     }
     return items;
   }
 
-  void _changedDropDownItem(String selectedFruit) {
+  void _changedDropDownItem(String selectedCategory) {
     setState(() {
-      _seletectedFruit = selectedFruit;
+      _seletectedCategory = selectedCategory;
+      print("category: " + _seletectedCategory);
     });
   }
 
-  void _add() {
+  void _add(String categoryId, String title, String content) {
     Map<String, String> data = <String, String>{
-      "categoryId": "2",
+      "categoryId": categoryId,
       "id": "",
-      "title": "New Content",
-      "content": "This is new content bla bla bla",
+      "title": title,
+      "content": content,
       "image": ""
     };
     collectionReference
@@ -64,13 +70,12 @@ class PostContentState extends State<PostContent> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Admin'),
-      ),
-      body: SingleChildScrollView(
-        child: _buildBody(context),
-      )
-    );
+        appBar: new AppBar(
+          title: new Text('Admin'),
+        ),
+        body: SingleChildScrollView(
+          child: _buildBody(context),
+        ));
   }
 
   Widget _buildBody(BuildContext context) {
@@ -93,6 +98,7 @@ class PostContentState extends State<PostContent> {
 
   Widget titleField(BuildContext context) {
     return TextField(
+      controller: titleTextController,
       keyboardType: TextInputType.text,
       decoration: InputDecoration(
         hintText: 'Tiêu đề',
@@ -107,7 +113,7 @@ class PostContentState extends State<PostContent> {
       new Padding(
         padding: EdgeInsets.all(10.0),
         child: new DropdownButton(
-          value: _seletectedFruit,
+          value: _seletectedCategory,
           items: _dropDownMenuItems,
           onChanged: _changedDropDownItem,
         ),
@@ -117,6 +123,7 @@ class PostContentState extends State<PostContent> {
 
   Widget contentField(BuildContext context) {
     return TextField(
+      controller: contentTextController,
       keyboardType: TextInputType.multiline,
       maxLines: 5,
       decoration: InputDecoration(hintText: 'Nội dung', labelText: 'Nội dung'),
@@ -129,6 +136,7 @@ class PostContentState extends State<PostContent> {
     return RaisedButton(
         textColor: Colors.blue,
         child: Text('Đăng bài viết'),
-        onPressed: () => _add());
+        onPressed: () =>
+            _add(_seletectedCategory, titleTextController.text, contentTextController.text));
   }
 }
